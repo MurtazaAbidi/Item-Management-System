d67844a0ef2e2412f479cc869e525985ad95f1ca
Murtaza Abidi <murtaza2476175@gmail.com>
2023-04-16 00:38:04 +0500

    made a Modal for viewing each item details

--- src/components/ItemList/index.jsx
+++ src/components/ItemList/index.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ItemListStyles.css'
+import Modal from '../../Modal'
 
 const ItemList = ({ items }) => {
+    const [clickedItem, setClickedItem] = useState(null)
+    const [openModal, setOpenModal] = useState(false)
     return (
-        <div className='itemList-container'>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Price</th>
-                        <th >Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {items.map((element, index) => {
-                        return <tr key={index}>
-                            <td>{element.id}</td>
-                            <td>{element.name}</td>
-                            <td>{element.desc}</td>
-                            <td>Rs. {element.price}</td>
-                            <td><button>Update</button><button>Delete</button></td>
+        <>
+        {openModal?<Modal setOpenModal={setOpenModal} clickedItem={clickedItem} items={items} />:null}
+            <div className='itemList-container'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Price</th>
+                            <th >Actions</th>
                         </tr>
-                    })}
-                </tbody>
-            </table>
-        </div>
-
+                    </thead>
+                    <tbody>
+                        {items.map((element, index) => {
+                            return <tr key={index} onClick={()=>{setClickedItem(index),setOpenModal(true)}}>
+                                <td>{element.id}</td>
+                                <td>{element.name}</td>
+                                <td>{element.desc}</td>
+                                <td>Rs. {element.price}</td>
+                                <td><button>Update</button><button>Delete</button></td>
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
 
